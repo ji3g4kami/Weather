ca0810d5cfa4c9173eb7502d5c5af1f2c46c6822
吳登秝 <ji3g4kami@gmail.com>
2019-04-17 11:36:24 +0800

    [add] api tests

---
+++ WeatherTests/APITests.swift
@@ -0,0 +1,53 @@
+//
+//  APITests.swift
+//  WeatherTests
+//
+//  Created by udn on 2019/4/17.
+//  Copyright © 2019 dengli. All rights reserved.
+//
+
+import XCTest
+@testable import Weather
+
+class APITests: XCTestCase {
+
+    var urlSession: URLSession!
+    
+    override func setUp() {
+        super.setUp()
+        urlSession = URLSession(configuration: .default)
+    }
+    
+    override func tearDown() {
+        urlSession = nil
+        super.tearDown()
+    }
+    
+    func testCallToLondonWeatherCompletes() {
+        callTo(urlString: "https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22")
+    }
+    
+    func callTo(urlString: String) {
+        /// given
+        let url = URL(string: urlString)
+        let promise = expectation(description: "Completion handler invoked")
+        var statusCode: Int?
+        var responseError: Error?
+        
+        // when
+        let dataTask = urlSession.dataTask(with: url!) { (data, response, error) in
+            statusCode = (response as? HTTPURLResponse)?.statusCode
+            responseError = error
+            promise.fulfill()
+        }
+        dataTask.resume()
+        wait(for: [promise], timeout: 5)
+        
+        // then
+        XCTAssertNil(responseError)
+        XCTAssertEqual(statusCode, 200)
+    }
+}
+
+
+
